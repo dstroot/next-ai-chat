c70c68a4eb26fb4c14b4cb60360f5ceb61566dde
Dan Stroot <dan.stroot@gmail.com>
2025-02-05 14:22:48 -0800

    clean up UI

--- components/messages.tsx
+++ components/messages.tsx
@@ -1,7 +1,7 @@
 import { ChatRequestOptions, Message } from "ai";
 import { PreviewMessage, ThinkingMessage } from "./message";
 import { useScrollToBottom } from "./use-scroll-to-bottom";
-import { Overview } from "./overview";
+// import { Overview } from "./overview";
 import { memo } from "react";
 import { Vote } from "@/lib/db/schema";
 import equal from "fast-deep-equal";
@@ -38,7 +38,7 @@ function PureMessages({
       ref={messagesContainerRef}
       className="flex flex-col min-w-0 gap-6 flex-1 overflow-y-scroll pt-4"
     >
-      {messages.length === 0 && <Overview />}
+      {/* {messages.length === 0 && <Overview />} */}
 
       {messages.map((message, index) => (
         <PreviewMessage
